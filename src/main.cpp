e81689d455157e0dc1150f612b5a73a073cfe7fa
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-13 16:55:58 +0530

    Added header in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,23 @@
-/*
-Copyright (c) 2014, Nghia Ho
-Copyright (c) 2016 -, Dilawar Singh  <dilawars@ncbs.res.in>
+/***
+ *       Filename:  main.cpp
+ *
+ *    Description:  description
+ *
+ *        Version:  0.0.1
+ *        Created:  2016-10-13
+ *       Revision:  none
+ *
+ *         Author:  Dilawar Singh <dilawars@ncbs.res.in>
+ *   Organization:  NCBS Bangalore
+ *
+ *        License:  GNU GPL2
+ *
+ *   NOTE :
+ *
+ *   This application is built over code posted by Nghia Ho on his blog
+ *   http://nghiaho.com/?p=2093 (accessed Oct 13, 2016). 
+ **/
 
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software without
-specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
---------------------------------------------------------------------------------
-Modification log:
-
-    Wed 12 Oct 2016 10:59:23 AM IST , Dilawar Singh
-
-        - Compiled with opencv-3 on openSUSE-Tumbleweed.
-        - Added CMake support.
-        - Added tiff support.
-        - 
-*/
  
 #include <opencv2/opencv.hpp>
 #include <iostream>
@@ -48,8 +27,6 @@ Modification log:
 #include "videoio.hpp"
 #include "motion_stabilizer.hpp"
 #include "tclap/CmdLine.h"
-
-
 
 using namespace std;
 using namespace cv;
@@ -81,8 +58,11 @@ int main(int argc, char **argv)
         cmd.add( outputArg );
 
         TCLAP::ValueArg<size_t> numpassArg ("n"
-                , "num-passes" , "Number of passes to perform to stabilize video."
-                " (default 2). Increase this number for better result."
+                , "num-passes" 
+                , "Number of passes to perform to stabilize video."
+                " (default 2). Increase this number for better result.\n"
+                " On normal recording, 1 would be sufficient but on recording "
+                " where features are small, you should increase it "
                 , false , 2 , "postitive integer"
                 );
         cmd.add( numpassArg );
